f36f55ae863c24ab82b39f46fb6589edeff48a93
hojun <hojunyee0328@gmail.com>
2025-12-11 20:02:59 +0900

    cd 수정

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,8 +2,13 @@
 
 TARGET="$1"
 
-cd /home/ec2-user/deploy
+cd /home/ec2-user/tech-gather
 export DOCKER_REPO="${DOCKER_REPO}"
 
+echo "Pulling image.. "
 docker-compose pull $TARGET
+
+echo "Restarting service..."
 docker-compose up -d $TARGET
+
+echo "Deployment completed"
