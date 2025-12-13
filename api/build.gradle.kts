32dcac722ef633ed06c9025de45ce51ab5ba8b8a
hojun <hojunyee0328@gmail.com>
2025-12-13 21:38:04 +0900

    boot 버전 변경

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -11,8 +11,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 
-	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
-    implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation ("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
+	implementation("org.springframework.boot:spring-boot-starter-security")
     testImplementation("org.springframework.security:spring-security-test")
 
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
