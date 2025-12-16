270ed655404f22e29a8512e3f1edd53ba3877bb7
hojun <hojunyee0328@gmail.com>
2025-12-16 17:48:27 +0900

    springdoc 버전 업

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -12,7 +12,9 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 
-	implementation ("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
+//	implementation ("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
+	implementation ("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
+
 	implementation("org.springframework.boot:spring-boot-starter-security")
     testImplementation("org.springframework.security:spring-security-test")
 
